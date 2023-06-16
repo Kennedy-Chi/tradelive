bef6d866493dc41581153017c8ac157271b6e351
Kennedy-Chidi <kennedychidi3@gmail.com>
2023-06-16 10:09:03 +0100

    good

--- dist/script/smartSupp.js
+++ dist/script/smartSupp.js
@@ -17,14 +17,21 @@
 //     s.parentNode.insertBefore(c, s);
 //   })(document);
 
-var Tawk_API = Tawk_API || {},
-  Tawk_LoadStart = new Date();
-(function () {
-  var s1 = document.createElement("script"),
-    s0 = document.getElementsByTagName("script")[0];
-  s1.async = true;
-  s1.src = "https://embed.tawk.to/646eae1574285f0ec46d79bb/1h185g1ao";
-  s1.charset = "UTF-8";
-  s1.setAttribute("crossorigin", "*");
-  s0.parentNode.insertBefore(s1, s0);
-})();
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "cd2ca336db1d609987da8e6e340aa2468a458ff2";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
