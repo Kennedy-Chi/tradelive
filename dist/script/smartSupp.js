499e5f0aa1a1db3f3d3a722f9ad7a9699824fda4
Kennedy-Chidi <kennedychidi3@gmail.com>
2023-07-29 11:39:41 +0100

    nice

--- dist/script/smartSupp.js
+++ dist/script/smartSupp.js
@@ -1,24 +1,5 @@
-// var _smartsupp = _smartsupp || {};
-// _smartsupp.key = "a9f9481df5104797a141789d469a4e263dccc79e";
-// window.smartsupp ||
-//   (function (d) {
-//     var s,
-//       c,
-//       o = (smartsupp = function () {
-//         o._.push(arguments);
-//       });
-//     o._ = [];
-//     s = d.getElementsByTagName("script")[0];
-//     c = d.createElement("script");
-//     c.type = "text/javascript";
-//     c.charset = "utf-8";
-//     c.async = true;
-//     c.src = "https://www.smartsuppchat.com/loader.js?";
-//     s.parentNode.insertBefore(c, s);
-//   })(document);
-
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = "cd2ca336db1d609987da8e6e340aa2468a458ff2";
+_smartsupp.key = "1ee2b929eb42c9e3977b2884c2daf96711078905";
 window.smartsupp ||
   (function (d) {
     var s,
